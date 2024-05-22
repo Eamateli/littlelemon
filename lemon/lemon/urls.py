bbe842730abf57c3d2ce93714b751743995c639f
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-22 20:17:13 +0300

    Adding urls, view and debugging

--- lemon/lemon/urls.py
+++ lemon/lemon/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/',include('resaurant.urls')),
+    path('restaurant/',include('restaurant.urls')),
     
 ]
